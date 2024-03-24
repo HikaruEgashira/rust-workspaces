4dd858e5d2791628e655112892d9f5c091387bd1
hikae <contact@egahika.dev>
2024-03-24 14:55:54 +0900

    init 2.4

---
+++ kani-tutorial/loops-unwinding/src/lib.rs
@@ -0,0 +1,22 @@
+#[allow(dead_code)]
+fn initialize_prefix(length: usize, buffer: &mut [u8]) {
+    if length > buffer.len() {
+        return;
+    }
+
+    for i in 0..=length {
+        buffer[i] = 0;
+    }
+}
+
+#[cfg(kani)]
+#[kani::proof]
+fn check_initialize_prefix() {
+    const LIMIT: usize = 10;
+    let mut buffer: [u8; LIMIT] = [1; LIMIT];
+
+    let length = kani::any();
+    kani::assume(length <= LIMIT);
+
+    initialize_prefix(length, &mut buffer);
+}
