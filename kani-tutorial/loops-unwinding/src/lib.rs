d1d767f6e20cac3c15664be450c35a1ea9bedd3d
hikae <contact@egahika.dev>
2024-03-24 14:56:05 +0900

    solve 2.4

--- kani-tutorial/loops-unwinding/src/lib.rs
+++ kani-tutorial/loops-unwinding/src/lib.rs
@@ -11,6 +11,7 @@ fn initialize_prefix(length: usize, buffer: &mut [u8]) {
 
 #[cfg(kani)]
 #[kani::proof]
+#[kani::unwind(11)]
 fn check_initialize_prefix() {
     const LIMIT: usize = 10;
     let mut buffer: [u8; LIMIT] = [1; LIMIT];
@@ -20,3 +21,11 @@ fn check_initialize_prefix() {
 
     initialize_prefix(length, &mut buffer);
 }
+
+// kani::unwind(1)
+// Failed Checks: unwinding assertion loop 0
+//  File: "kani-tutorial/loops-unwinding/src/lib.rs", line 7, in initialize_prefix
+
+// kani::unwind(11)
+// Failed Checks: index out of bounds: the length is less than or equal to the given index
+//  File: "kani-tutorial/loops-unwinding/src/lib.rs", line 8, in initialize_prefix
